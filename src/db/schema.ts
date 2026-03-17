7681880b3f5a814e69d4cc08f9ee8a92b19ed762
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 11:53:26 -0700

    config drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,15 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const tasks = sqliteTable("tasks", {
+  id: integer({ mode: "number" })
+    .primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  done: integer({ mode: "boolean" })
+    .notNull()
+    .default(false),
+  createdAt: integer({ mode: "timestamp" })
+    .$defaultFn(() => new Date()),
+  updatedAt: integer({ mode: "timestamp" })
+    .$defaultFn(() => new Date())
+    .$onUpdate(() => new Date()),
+});
