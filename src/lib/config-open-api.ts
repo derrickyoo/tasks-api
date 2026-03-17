649bbd59dbd7a3b559fc853193e64dfad4cda39b
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 09:28:33 -0700

    add scalar

--- src/lib/config-open-api.ts
+++ src/lib/config-open-api.ts
@@ -1,3 +1,5 @@
+import { Scalar } from "@scalar/hono-api-reference";
+
 import type { AppOpenAPI } from "./types";
 
 import packageJSON from "../../package.json";
@@ -10,4 +12,12 @@ export default function configOpenAPI(app: AppOpenAPI) {
       title: "Tasks API",
     },
   });
+
+  app.get(
+    "/reference",
+    Scalar({
+      url: "/doc",
+      theme: "purple",
+    }),
+  );
 }
