b01014f761e83c9bc74ba4f13a4bb199c9551193
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 08:28:15 -0700

    add openapi docs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import createApp from "@/lib/create-app";
 
+import configOpenAPI from "./lib/config-open-api";
+
 const app = createApp();
+configOpenAPI(app);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
