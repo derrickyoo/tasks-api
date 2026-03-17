f0e3b5d9530738107a5dfda2e2e8f82cd4701240
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 10:44:55 -0700

    task list handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import configOpenAPI from "@/lib/config-open-api";
 import createApp from "@/lib/create-app";
 import index from "@/routes/index.route";
+import tasks from "@/routes/tasks/tasks.index";
 
 const app = createApp();
 
 const routes = [
   index,
+  tasks,
 ];
 
 configOpenAPI(app);
