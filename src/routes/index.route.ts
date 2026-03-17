c662de2055aecebb3550dc9d7047f6ce9c93d46d
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 10:10:15 -0700

    update

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,19 +1,19 @@
-import { createRoute, z } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 import { createRouter } from "@/lib/create-app";
 
 const router = createRouter();
 
 router.openapi(createRoute({
+  tags: ["Index"],
   method: "get",
   path: "/",
   responses: {
     [HttpStatusCodes.OK]: jsonContent(
-      z.object({
-        message: z.string(),
-      }),
+      createMessageObjectSchema("Tasks API"),
       "Tasks API Index",
     ),
   },
