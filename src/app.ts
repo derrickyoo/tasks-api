610b9725c26db0251b0817c53aacaca3409b4cf8
Derrick Yoo <derrickyoo@gmail.com>
2026-03-16 21:24:16 -0700

    init basic middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/error", () => {
+  throw new Error("Error route");
+});
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
