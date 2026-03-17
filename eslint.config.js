8d5e4fa1c89c21ec8ea3764b3c061ac03c516c50
Derrick Yoo <derrickyoo@gmail.com>
2026-03-16 20:37:32 -0700

    config eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: ["**/migrations/*"],
+});
