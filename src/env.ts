770e11c6adcc03d2a4469e3cab8d32f9eb30ed15
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 07:34:36 -0700

    config env

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,16 @@
-export default {};
+/* eslint-disable node/no-process-env */
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z } from "zod";
+
+expand(config());
+
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  PORT: z.coerce.number().default(9999),
+  LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"]),
+});
+
+const env = EnvSchema.parse(process.env);
+
+export default env;
