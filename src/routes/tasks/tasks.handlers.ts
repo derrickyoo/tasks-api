c3a20cf82fbc95120d239eef2ed40c68f5c26647
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 10:58:55 -0700

    register tasks list

--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,7 +1,7 @@
 import type { AppRouteHandler } from "@/lib/types";
 import type { ListRoute } from "@/routes/tasks/tasks.routes";
 
-export const handlerList: AppRouteHandler<ListRoute> = (c) => {
+export const list: AppRouteHandler<ListRoute> = (c) => {
   return c.json([{
     name: "Learn Hono",
     done: false,
--- src/routes/tasks/tasks.index.ts
+++ src/routes/tasks/tasks.index.ts
@@ -1,5 +1,8 @@
 import { createRouter } from "@/lib/create-app";
+import * as handlers from "@/routes/tasks/tasks.handlers";
+import * as routes from "@/routes/tasks/tasks.routes";
 
-const router = createRouter();
+const router = createRouter()
+  .openapi(routes.list, handlers.list);
 
 export default router;
