259d797e3e35e14df29ce8051ba75d2df44c7725
Derrick Yoo <derrickyoo@gmail.com>
2026-03-17 09:58:03 -0700

    add default hook to handle validation errors across routes

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { requestId } from "hono/request-id";
 import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
+import { defaultHook } from "stoker/openapi";
 
 import type { AppBindings } from "@/lib/types";
 
@@ -9,6 +10,7 @@ import { pinoLogger } from "@/middleware/pino-logger";
 export function createRouter() {
   const app = new OpenAPIHono<AppBindings>({
     strict: false,
+    defaultHook,
   });
 
   return app;
